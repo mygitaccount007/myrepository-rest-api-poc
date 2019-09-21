3a6dd10166605c2a3edb03ea673b13acee2518c2
mygitaccount007 <53487923+mygitaccount007@users.noreply.github.com>
2019-09-21 17:30:22 +0530

    Update item.py

--- models/item.py
+++ models/item.py
@@ -5,18 +5,20 @@ class ItemModel(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80))
+    description = db.Column(db.String(200))
     price = db.Column(db.Float(precision=2))
 
     store_id = db.Column(db.Integer, db.ForeignKey('stores.id'))
     store = db.relationship('StoreModel')
 
-    def __init__(self, name, price, store_id):
+    def __init__(self, name, price, description, store_id):
         self.name = name
         self.price = price
+        self.description = description
         self.store_id = store_id
 
     def json(self):
-        return {'name': self.name, 'price': self.price}
+        return {'name': self.name, 'price': self.price,'description': self.description}
 
     @classmethod
     def find_by_name(cls, name):
